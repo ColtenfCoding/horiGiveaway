6241f6d95dc0c6f8101f47bb527fc5c83529c2b9
ColtenfCoding <81717145+ColtenfCoding@users.noreply.github.com>
2021-04-22 16:32:03 -0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -6,7 +6,7 @@ module.exports = (client) => {
   const activities = [`Giveaways in ${client.guilds.cache.size} guilds`,"g!help",`over ${client.users.cache.size} users!`];
   setInterval(() => {
     let activity = activities[Math.floor(Math.random() * activities.length)];
-    client.user.setActivity("thuy to die", { type: "WATCHING" });
+    client.user.setActivity("GIVEAWAYS | CAPTAIN | JOIN THE SUPPORT SERVER!", { type: "STREAMING" });
   }, 20000);
 
 };
